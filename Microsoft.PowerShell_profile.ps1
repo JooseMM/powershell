3884bfad9d5843317f4c505fba6617933dfb4260
Jose Moreno <jamm.webdev@gmail.com>
2025-09-16 00:44:30 -0300

    add CODE_DIRECTORY enviroment variable

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -13,14 +13,11 @@ Remove-Item Alias:ls
 # Aliases
 Set-Alias ls "eza"
 
-function Code-Directory {
- Set-Location "L:\code"
-}
-
 # Enviroment Variables
 $env:CONFIG_DIRECTORY = "C:\Users\josex\.config"
-
 # Starship Config Directory
 $env:STARSHIP_CONFIG = "$env:USERPROFILE\.config\starship\starship.toml"
 # Neovim Config Directory
 $env:XDG_CONFIG_HOME = "$env:USERPROFILE\.config\"
+# Code Directory
+$env:CODE_DIRECTORY = "L:\code"
